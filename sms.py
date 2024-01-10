54589e1682182217fd77f2010268b0cbcffe488b
Walke <walkersgotyou@gmail.com>
2024-01-09 19:10:41 -0500

    first steps

---
+++ sms.py
@@ -0,0 +1,9 @@
+
+# the basic setup
+import requests
+resp = requests.post('https://textbelt.com/text', {
+  'phone': '5555555555',
+  'message': 'Hello world',
+  'key': 'textbelt',
+})
+print(resp.json())
