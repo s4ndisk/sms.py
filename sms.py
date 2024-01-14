18f0f7981d00008493193de57beb5a992ffab3bc
kok4i <walkersgotyou@gmail.com>
2024-01-14 00:59:26 -0500

    Base Script

--- sms.py
+++ sms.py
@@ -1,9 +1,37 @@
+# script imports
+import argparse
+
+# initialize parser
+parser = argparse.ArgumentParser()
+
+# adding phone argument
+parser.add_argument(
+   'phone', 
+   help = 'Specify phone number (US/Canada Only)'
+   )
+
+# adding phone argument
+parser.add_argument(
+   'message', 
+   help = 'Specify message to send (Max 160 characters)'
+   )
+
+# adding API key argument
+parser.add_argument(
+   '-k',
+   '--key', 
+   default='textbelt', 
+   help = 'Specifiy API key',
+   )
+
+# Read arguments from command line
+args = parser.parse_args()
 
 # the basic setup
 import requests
 resp = requests.post('https://textbelt.com/text', {
-  'phone': '5555555555',
-  'message': 'Hello world',
-  'key': 'textbelt',
+  'phone': args.phone,
+  'message': args.message,
+  'key': args.key,
 })
 print(resp.json())
